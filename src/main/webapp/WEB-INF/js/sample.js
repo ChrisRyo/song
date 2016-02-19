d7612b2b1daddb7f9a94f04dac84d91eea911f8e
ChrisRyo <chrisryo0301@gmail.com>
2016-02-19 22:02:44 +0800

    test

---
+++ src/main/webapp/WEB-INF/js/sample.js
@@ -0,0 +1,26 @@
+$( document ).ready(function() {
+	
+    initSample();
+});
+
+
+function initSample () {
+	
+	$("#put").click(function(){
+		$.ajax({
+	        type: 'GET',
+	        url: "/sample",
+	        dataType: "json", // data type of response
+	        
+	        success: function(json){
+	        	$("#aaa").val(json.name);
+	        },
+
+	         error:function(xhr, ajaxOptions, thrownError){ 
+	            alert(xhr.status); 
+	            alert(thrownError); 
+	         }
+	    });
+	});
+	
+}
---
+++ src/main/webapp/WEB-INF/jsp/sample.jsp
@@ -0,0 +1,17 @@
+<%@page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<html>
+<head>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+<script type="text/javascript" src="/test3/src/main/webapp/WEB-INF/js/sample.js"></script>
+</head>
+<body>
+	<table>
+		<tr>
+		<td>
+			<input type = "text" id="aaa" value="ffffffff">
+			<input type = "button" id="put" value= "put">
+		</td>
+		</tr>
+	</table>
+</body>
+</html>
