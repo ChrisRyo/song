a3b0b4b4145650629c0c65d7aecdaecb87c0b79c
ChrisRyo <chrisryo0301@gmail.com>
2016-02-20 17:15:53 +0800

    Filter setting

--- src/main/java/tw/com/MyApplication.java
+++ src/main/java/tw/com/MyApplication.java
@@ -2,11 +2,16 @@ package tw.com;
 
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.server.mvc.jsp.JspMvcFeature;
+
+import tw.com.logic.filter.MyFilter;
  
 public class MyApplication extends ResourceConfig{
     public MyApplication(){
     	packages("tw.com.view.resource");
     	packages("tw.com.webservice");
+    	// Filter
+    	register(MyFilter.class);
+    	// jsp
         register(JspMvcFeature.class);
         property(JspMvcFeature.TEMPLATE_BASE_PATH, "/WEB-INF/jsp");
     }
